89e742f8c5edd0b325e29975f8bcc9849d545335
Ryan Cloutier <cloutier@astro.utoronto.ca>
2018-09-11 14:51:36 -0400

    save stellar parameters in results class

--- compile_results.py
+++ compile_results.py
@@ -15,6 +15,8 @@ class K2results:
 	fs = np.array(glob.glob('%s/%s*/*'%(self.folder, self.prefix)))
 	self.fs = []
 	self.Ndetected, self.params_guess = np.zeros(0), np.zeros((0,4))
+        self.Kepmags, self.Mss, self.Rss, self.Teffs = np.zeros(0), np.zeros(0), \
+                                                       np.zeros(0), np.zeros(0)
 	for i in range(fs.size):
 	    print float(i)/fs.size
 	    d = loadpickle(fs[i])
@@ -22,6 +24,10 @@ class K2results:
 		Ndet = int(d.params_guess.shape[0])
 		for j in range(Ndet):
 		    self.fs.append(fs[i])
+                    self.Kepmags = np.append(self.Kepmags, d.Kepmag)
+                    self.Mss = np.append(self.Mss, d.Ms)
+                    self.Rss = np.append(self.Rss, d.Rs)
+                    self.Teffs = np.append(self.Teffs, d.Teff)
 		    self.Ndetected = np.append(self.Ndetected, Ndet)
 		    self.params_guess = np.append(self.params_guess, d.params_guess, axis=0)
   	
