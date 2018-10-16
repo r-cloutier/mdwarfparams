b74ad663e8582325eedc869c4846229ff0b6cef5
Ryan Cloutier <cloutier@astro.utoronto.ca>
2018-10-16 11:37:45 -0400

    updated compile_results to save all planets detected in the 'planetsearch'

--- compile_results.py
+++ compile_results.py
@@ -3,22 +3,24 @@ from K2LCclass import *
 
 class K2results:
 
-    def __init__(self, folder, prefix):
-	self.folder, self.prefix = folder, prefix
-	self.fname_out = '%s/%sK2results'%(self.folder, self.prefix)
+    def __init__(self, folder):
+	self.folder = folder
+	self.fname_out = '%s/EPIC_K2results'%self.folder
 	
 	self.get_results()
 	self._pickleobject()
 
 
     def get_results(self):
-	fs = np.array(glob.glob('%s/%s*/*'%(self.folder, self.prefix)))
+	fs = np.array(glob.glob('%s/EPIC_*/K2LC_-00099'%self.folder))
 	self.fs, self.epicnames = [], np.zeros(0)
 	self.Ndetected, self.params_guess, self.cond_vals = np.zeros(0), np.zeros((0,4)), \
                                                             np.zeros((0,5))
-        self.Kepmags, self.Mss, self.Rss, self.Teffs = np.zeros(0), np.zeros(0), \
-                                                       np.zeros(0), np.zeros(0)
-	self.efs = np.zeros(0)
+        self.Kepmags, self.efs = np.zeros(0), np.zeros(0)
+	self.Mss, self.e_Mss = np.zeros(0), np.zeros(0)
+        self.Rss, self.e_Rss = np.zeros(0), np.zeros(0)
+        self.Teffs, self.e_Teffs = np.zeros(0), np.zeros(0)
+        self.loggs, self.e_loggs = np.zeros(0), np.zeros(0)
 	for i in range(fs.size):
 	    print float(i)/fs.size
 	    d = loadpickle(fs[i])
@@ -28,10 +30,13 @@ class K2results:
 		    self.fs.append(fs[i])
 		    self.epicnames = np.append(self.epicnames, int(fs[i].split('_')[1].split('/')[0]))
                     self.Kepmags = np.append(self.Kepmags, d.Kepmag)
+                    self.efs = np.append(self.efs, d.ef.mean())
                     self.Mss = np.append(self.Mss, d.Ms)
+		    self.e_Mss = np.append(self.e_Mss, d.e_Ms)
                     self.Rss = np.append(self.Rss, d.Rs)
+                    self.e_Rss = np.append(self.e_Rss, d.e_Rs)
                     self.Teffs = np.append(self.Teffs, d.Teff)
-		    self.efs = np.append(self.efs, d.ef.mean())
+                    self.e_Teffs = np.append(self.e_Teffs, d.e_Teff)
 		    self.Ndetected = np.append(self.Ndetected, Ndet)
                     
                     params = d.params_guess[j-1] if j > 0 else np.repeat(np.nan,4)
@@ -64,5 +69,5 @@ class K2results:
 
 
 if __name__ == '__main__':
-    folder, prefix = sys.argv[1], sys.argv[2]  # PipelineResults EPIC_
-    self = K2results(folder, prefix)
+    folder = sys.argv[1]  # PipelineResults
+    self = K2results(folder)
